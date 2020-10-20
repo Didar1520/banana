02f0cc8b9477f52009849d0884f47c92613be025
Didar1520 <72905526+Didar1520@users.noreply.github.com>
2020-10-20 16:46:15 +0600

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+name = prompt('введи ваше имя')
+surname = prompt('введите вашу фамилию')
+
+document.queryselectorAll(.button )
+
+
+
+alert('поздравляю' + ' ' + name + ' ' + surname + ' ' + 'вы выиграли iphone X' )
